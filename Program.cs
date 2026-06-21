32a6a3634849c28304f87027cdbbff53bf264a61
Luukmania <your-github-email@example.com>
2026-06-21 17:58:17 +0100

    Initial EExam API

---
+++ Program.cs
@@ -0,0 +1,10 @@
+using System;
+class Program {
+    static void Main() {
+        Console.WriteLine("Hello from Mono on Android!");
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Nice to meet you, " + name + "!");
+    }
+}
+
